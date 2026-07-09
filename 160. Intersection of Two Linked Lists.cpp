b5dd312be0e1a7098dcbfbd27600a33f1a764313
Utpal Raj <utpalraj1609@gmail.com>
2026-07-09 21:25:49 +0530

    Implement intersection detection for two linked lists

---
+++ 160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        ListNode *ptrA = headA;
+        ListNode *ptrB = headB;
+
+        while(ptrA != ptrB)
+        {
+            if (ptrA == NULL)
+            {
+                ptrA = headB;
+            }
+            else
+            {
+                ptrA = ptrA->next;
+            }
+            if (ptrB == NULL)
+            {
+                ptrB = headA;
+            }
+            else
+            {
+                ptrB = ptrB->next;
+            }
+        }
+        return ptrA;
+    }
+};
